ddee699308a043d8e10ffaba36e085fc7c5b5ce9
Tomas Pokstefl <poksteflt@gmail.com>
2024-06-01 11:12:57 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import data from './data/mock.json' assert {type: "json"};
+
+const app = express();
+const PORT = 3000;
+
+//GET
+app.get("/", (request, response) => {
+    response.json(data)
+})
+//CREATE
+app.post("/create", (request, response) => {
+    response.send("This is a POST request at /create:");
+})
+//PUT
+app.put("/edit", (request, response) =>{
+    response.send("This is a PUT request at /edit")
+})
+//DELETE
+app.delete("/delete", (request, response) => {
+    response.send("This is a DELETE request at /delete");
+})
+
+app.listen(PORT, () => {
+    console.log(`The server is running on port ${PORT}`)
+    console.log(data);
+});
